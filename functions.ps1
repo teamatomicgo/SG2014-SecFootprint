90411d3512cba6e18c7a30184e20ca58591d78a3
Eric Courville <orgnaicit-git@outlook.com>
2014-01-27 22:58:19 -0800

    moved functions to seperate ps1 file

---
+++ functions.ps1
@@ -0,0 +1,27 @@
+﻿
+function Invoke-SecurityScan([string[]]$computername) {
+	
+	Write-Output $computername
+	Get-MountPoints $computername
+	Get-EnvVars $computername
+	
+}
+
+function Get-MountPoints ([string]$computername) {
+	$HostDrives = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computername -Filter "DriveType=3" | ForEach-Object {$_.DeviceID}
+	$HostShares = Get-WmiObject -Class Win32_Share -ComputerName $computername
+	Write-Output $HostDrives
+	Write-Output $HostShares
+}
+
+function Get-EnvVars ([string]$computername) {
+	$HostEnvVars = Get-WmiObject -Class Win32_Environment -ComputerName $computername
+	Write-Output $HostEnvVars
+}
+
+function Get-HostProcesses ( [string]$computername) {
+	
+	$HostProcs = Get-Process
+	# wrap this up into expanded object as needed.
+	
+}
--- main.ps1
+++ main.ps1
@@ -1,27 +1,2 @@
 ﻿
-
-
-function Invoke-SecurityScan([string[]]$computername) {
-	
-	Write-Output $computername
-	Get-MountPoints $computername
-	Get-EnvVars $computername
-	
-}
-
-function Get-MountPoints ([string]$computername) {
-	$HostDrives = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computername -Filter "DriveType=3" | ForEach-Object {$_.DeviceID}
-	$HostShares = Get-WmiObject -Class Win32_Share -ComputerName $computername
-	Write-Output $HostDrives
-	Write-Output $HostShares
-}
-
-function Get-EnvVars ($computername) {
-	$HostEnvVars = Get-WmiObject -Class Win32_Environment -ComputerName $computername
-	Write-Output $HostEnvVars
-}
-
-function Get-HostShares ($co) {
-	
-}
 Invoke-SecurityScan "."
